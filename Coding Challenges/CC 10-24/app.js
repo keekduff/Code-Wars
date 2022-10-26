ec2045b263c78ed5251fc5b8fef866b8a2e7b53c
keekduff <81106815+keekduff@users.noreply.github.com>
2022-10-25 22:47:36 -0400

    update

--- Coding Challenges/CC 10-24/app.js
+++ Coding Challenges/CC 10-24/app.js
@@ -44,4 +44,19 @@ function wolvesEatSheep(arr) {
   }
   console.log(wolvesEatSheep(["sheep", "sheep", "sheep", "sheep", "sheep", "sheep","wolf"]))
 
-// P4
+// P4 Tomorrow's code
+// We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+// Our team's match results are recorded in a collection of strings. Each match is represented by a string in the format "x:y", where x is our team's score and y is our opponents score.
+
+function points(games) {
+  let total = 0;
+  games.map(game => {
+    if (game[0] === game[2]) {
+      total += 1;
+    } else if (game[0] > game[2]) {
+      total += 3;
+    }
+  });
+  return total;
+}
+console.log(points(10))
